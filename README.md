e0a04065fd1e27a5216a7880d6f3f8fc9d4349f2
Caio Diomedes de Alcântara Braga <164583236+caiodiom@users.noreply.github.com>
2024-09-05 10:50:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# imersao
+ imersao-alura
---
+++ dados.js
@@ -0,0 +1,105 @@
+let dados = [
+    {
+        titulo: "Xiaomi",
+        descricao:
+                `
+                    <ul>
+                        <li>Xiaomi Civic</li>
+                        <li>Xiaomi 12X</li>
+                        <li>Xiaomi 11i</li>
+                        <li>Xiaomi 11i HyperCharge 5G</li>
+                        <li>Xiaomi 11 Lite 5G</li>
+                        <li>Xiaomi Pad 5 Pro</li>
+                        <li>Xiaomi Mix 4</li>
+                        <li>Smartphones Xiaomi mais antigos</li>
+                        </ul>
+                `
+        
+        ,
+        link: "https://pt.wikipedia.org/wiki/Android_15"
+    },
+
+    {
+        titulo: "Google Pixel",
+        descricao:
+                `
+                    <ul>
+                        <li>Pixel 5</li>
+                        <li>Pixel 5a 5G</li>
+                        <li>Pixel 4</li>
+                        <li>Pixel 4XL</li>
+                        <li>Pixel 4a</li>
+                        <li>Pixel 4a 5G</li>
+                        <li>Pixel 3</li>
+                        <li>Pixel 3XL</li>
+                        <li>Pixel 3a</li>
+                        <li>Pixel 3aXL</li>
+                        <li>Smartphones Pixel mais antigos</li>
+                      </ul>
+                `
+        
+        ,
+        link: "https://pt.wikipedia.org/wiki/Android_15"
+    },
+
+    {
+        titulo: "Samsung",
+        descricao:
+                `
+                    <ul>
+                        <li>Galaxy S20 Ultra</li>
+                        <li>Galaxy S20+</li>
+                        <li>Galaxy S20 FE</li>
+                        <li>Modelos mais antigos da Série S</li>
+                        <li>Galaxy A72</li>
+                        <li>Galaxy A52</li>
+                        <li>Galaxy A52 5G</li>
+                        <li>Galaxy A52s 5G</li>
+                        <li>Galaxy A32</li>
+                        <li>Galaxy A32 5G</li>
+                        <li>Galaxy Z Fold 2</li>
+                        <li>Galaxy Z Flip 2</li>
+                        <li>Modelos mais antigos da Série Z</li>
+                      </ul>
+                `
+        
+        ,
+        link: "https://pt.wikipedia.org/wiki/Android_15"
+    },
+
+    {
+        titulo: "Motorola",
+        descricao:
+                `
+                    <ul>
+                        <li>Motorola Razr (2022)</li>
+                        <li>Motorola Razr 5G</li>
+                        <li>Todos os modelos RAZR mais antigos</li>
+                        <li>Motorola Edge 30 Fusion</li>
+                        <li>Motorola Edge 30 Neo</li>
+                        <li>Motorola Edge (2022)</li>
+                        <li>Motorola Edge+ 5G UW (2022)</li>
+                        <li>Motorola Edge 30 Pro</li>
+                        <li>Todos os modelos EDGE mais antigos</li>
+                        <li>Motorola Moto G84</li>
+                        <li>Motorola Moto G73</li>
+                        <li>Motorola Moto G54</li>
+                        <li>Motorola Moto G53</li>
+                        <li>Motorola Moto G54 Power</li>
+                        <li>Motorola Moto G23</li>
+                        <li>Motorola Moto G14</li>
+                        <li>Motorola Moto G13</li>
+                        <li>Motorola Moto G Stylus 5G (2023)</li>
+                        <li>Motorola Moto G Stylus (2023)</li>
+                        <li>Motorola Moto G (2023)</li>
+                        <li>Motorola Moto G Power 5G</li>
+                        <li>Motorola Moto X40</li>
+                        <li>Todos os modelos mais antigos da Moto</li>
+                      </ul>
+                `
+        
+        ,
+        link: "https://pt.wikipedia.org/wiki/Android_15"
+    },
+
+];
